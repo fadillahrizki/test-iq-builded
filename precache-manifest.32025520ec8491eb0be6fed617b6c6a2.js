13fdf07ea9d9bc918b4658e655891addcff875f7
Rizky Fadillah <fadillahrizki16@gmail.com>
2020-11-01 16:15:17 +0700

    1 Nov

---
+++ precache-manifest.32025520ec8491eb0be6fed617b6c6a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8388f56f66b07bf496c1",
+    "url": "/css/app.00a4d7b2.css"
+  },
+  {
+    "revision": "022c5ae01090ca19ff13",
+    "url": "/css/chunk-vendors.1b5a44ad.css"
+  },
+  {
+    "revision": "fadc7e7eab9f87b94c7cfc10dd385f59",
+    "url": "/img/logo.fadc7e7e.png"
+  },
+  {
+    "revision": "f94be98b3a75d6ce7dbc362cc535f5de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8388f56f66b07bf496c1",
+    "url": "/js/app.132c184f.js"
+  },
+  {
+    "revision": "022c5ae01090ca19ff13",
+    "url": "/js/chunk-vendors.7e807977.js"
+  },
+  {
+    "revision": "ed53ef2d6f1a8197e925a6caf8069193",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
